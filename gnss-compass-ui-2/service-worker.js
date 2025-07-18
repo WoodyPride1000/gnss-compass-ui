5ad62fc19c81a52fcf14ebe341e0d414f7d047e7
WoodyPride1000 <wordy.rewind.7r@icloud.com>
2025-07-19 05:44:44 +0900

    Update service-worker.js

--- gnss-compass-ui-2/service-worker.js
+++ gnss-compass-ui-2/service-worker.js
@@ -1,1 +1,24 @@
-// service-worker.js 内容は省略
+const CACHE_NAME = "gnss-ui-cache-v1";
+const OFFLINE_URLS = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/utm.min.js",
+  "/tiles/"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(OFFLINE_URLS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
